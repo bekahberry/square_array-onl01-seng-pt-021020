ff2dc44efe7547d244a435bb3f4a8d5c1922b68b
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-17 02:05:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   numbers = [1, 2, 3]
-number.each do
+number.each do |numbers|
  while elements_taken_out < elements_in_numbers.each
  puts "#{numbers}**#{elements_in_numbers}"
  end
