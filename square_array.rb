9f5b015b2a371771785f895f7ae7aee45f44108a
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-17 02:02:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   numbers = [1, 2, 3]
  elements_in_numbers = numbers.size
  elements_taken_out = 0
- while elements_taken_out < elements_in_numbers 
+ while elements_taken_out < elements_in_numbers.each
  puts "#{numbers}**#{elements_in_numbers}"
  end
 end
